ff61839d90f6abb6434720dc7591baa4c2b64ec2
amaclarty <100883044+amaclarty@users.noreply.github.com>
2022-09-26 15:50:30 -0400

    added midi check

--- hydra-midi/src/App.js
+++ hydra-midi/src/App.js
@@ -10,7 +10,19 @@ import Hydraholder from './Hydraholder'
 // gradient(speed )
 // Outputs
 
+navigator.requestMIDIAccess()
+    .then(onMIDISuccess, onMIDIFailure);
 
+function onMIDISuccess(midiAccess) {
+    console.log(midiAccess);
+
+    var inputs = midiAccess.inputs;
+    var outputs = midiAccess.outputs;
+}
+
+function onMIDIFailure() {
+    console.log('Could not access your MIDI devices.');
+}
 
 function App() {
   
--- hydra-midi/src/Hydraholder.js
+++ hydra-midi/src/Hydraholder.js
@@ -1,5 +1,14 @@
+import { render } from "@testing-library/react";
 import React from "react";
 import { useState } from 'react'
+
+// osc (frequency, sync, offset)
+// noise(scale, offset)
+// shape(sides, radius, smoothing
+// solid(r, g, b, a )
+// voronoi( scale, speed, blending
+// gradient(speed )
+// Outputs
 
 const Hydra = require('hydra-synth')  
 const h = new Hydra({ makeGlobal: true, detectAudio: false }).synth
@@ -11,11 +20,30 @@ function Hydraholder()  {
     .rotate(0, 0.1)
     .mult(h.osc(10, 0.1).modulate(h.osc(20).rotate(0, -0.01), 0.1))
     .color(2.83, 0.91, 0.39)
+    
 
-    const osc2 = h.noise(10, 0.01, 1.4)
+    const osc2 = h.osc()
         .rotate(0, 0.1)
+        .colorama()
         .mult(h.osc(10, 0.1).modulate(h.osc(20).rotate(0, -0.01), 0.1))
         .color(2.83, 0.91, 0.39)
+        .modulate(osc1)
+        .colorama()
+        
+
+    const osc3 =h.voronoi(400, 0, 0)
+        .diff(h.voronoi(100, 2, 1))
+        .blend(h.osc(.83, 0.91, 0.39))
+        .mask(h.shape(300, .2))
+        .rotate(10, .1)
+        .diff(h.shape(420, .7))
+        .rotate(-8, 1)
+        .diff(h.shape(640.2, .5))
+        .diff(h.gradient())
+        .modulate(osc2)
+    
+    // const osc4 = osc1.mult(h.noise())
+        
 
     const [preset, setPreset] =useState(osc1)
 
@@ -25,6 +53,8 @@ function Hydraholder()  {
             <div>
             <button onClick={() => {setPreset(osc1)}}>Osc 1</button>
             <button onClick={() => {setPreset(osc2)}}>Osc2</button>
+            <button onClick={() => {setPreset(osc3)}}>Osc3</button>
+            {/* <button onClick={() => {setPreset(osc4)}}>Osc4</button> */}
             </div>
         </div>
     )
