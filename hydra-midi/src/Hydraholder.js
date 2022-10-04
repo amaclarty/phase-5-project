e9150cfd820be24b279cee5acf861b0c554fd923
amaclarty <100883044+amaclarty@users.noreply.github.com>
2022-10-04 14:23:26 -0400

    working MIDI connection in app.js and starter video

--- hydra-midi/src/Hydraholder.js
+++ hydra-midi/src/Hydraholder.js
@@ -1,72 +1,92 @@
-import { render } from "@testing-library/react";
-import React, { useEffect } from "react";
-import { useState } from 'react'
-import {useRef} from 'react'
+// import { render } from "@testing-library/react";
+// import React, { useEffect } from "react";
+// import { useState } from 'react'
+// import {useRef} from 'react'
 
-// osc (frequency, sync, offset)
-// noise(scale, offset)
-// shape(sides, radius, smoothing
-// solid(r, g, b, a )
-// voronoi( scale, speed, blending
-// gradient(speed )
-// Outputs
+// // osc (frequency, sync, offset)
+// // noise(scale, offset)
+// // shape(sides, radius, smoothing
+// // solid(r, g, b, a )
+// // voronoi( scale, speed, blending
+// // gradient(speed )
+// // Outputs
 
-const Hydra = require('hydra-synth')  
-const h = new Hydra({ makeGlobal: true, detectAudio: false }).synth
+// const Hydra = require('hydra-synth')  
+// const h = new Hydra({ makeGlobal: true, detectAudio: false }).synth
 
-function Hydraholder()  { 
-    const angles =useRef(4)
+// function Hydraholder()  { 
 
-    const osc1 = h.osc(10, 0.01, 1.4)
-    .rotate(0, 0.1)
-    .mult(h.osc(10, 0.1).modulate(h.osc(20).rotate(0, -0.01), 0.1))
-    .color(2.83, 0.91, 0.39)
+//     const osc1 = h.osc(10, 0.01, 1.4)
+//     .rotate(0, 0.1)
+//     .mult(h.osc(10, 0.1).modulate(h.osc(20).rotate(0, -0.01), 0.1))
+//     .color(2.83, 0.91, 0.39)
+
+//     const lfo = h.osc(1, 2.5, 1).kaleid(600)
     
 
-    const osc2 = h.osc()
-        .rotate(0, 0.1)
-        .colorama()
-        .mult(h.osc(10, 0.1).modulate(h.osc(20).rotate(0, -0.01), 0.1))
-        .color(2.83, 0.91, 0.39)
-        .modulate(osc1)
-        .colorama()
+//     const osc2 = h.osc(7, 0.03)
+//         .rotate(0, 0.1)
+//         // .contrast(slider1Value)
+//         .mult(h.osc(1, 0.1).modulate(h.osc(0.5).rotate(0, -0.01), 0.1))
+//         .color(2.83, 0.91, 0.39)
+//         // .modulate(lfo)
+//         .colorama()
+//         .kaleid(8)
+//         // .diff(h.noise().colorama())
         
 
-    const osc3 =h.voronoi(400, 0, 0)
-        .diff(h.voronoi(100, 2, 1))
-        .blend(h.osc(.83, 0.91, 0.39))
-        .mask(h.shape(300, .2))
-        .rotate(10, .1)
-        .diff(h.shape(420, .7))
-        .rotate(-8, 1)
-        .diff(h.shape(640.2, .5))
-        .diff(h.gradient())
-        .modulate(osc2)
-
-    const [rotate1, setRotate1] =useState(0.00)
-
-    const osc4 =h.shape(5)
-        .rotate(rotate1)
+//     const osc3 =h.voronoi(400, 0, 0)
+//         .diff(h.voronoi(100, 2, 1))
+//         .blend(h.osc(.83, 0.91, 0.39))
+//         .mask(h.shape(300, .2))
+//         .rotate(10, .1)
+//         .diff(h.shape(420, .7))
+//         .rotate(-8, 1)
+//         .diff(h.shape(640.2, .5))
+//         .diff(h.gradient())
         
 
-    const [preset, setPreset] =useState(osc1)
+    
+    
+
+//     const osc4 =h.osc(100)
+//         .diff(h.noise())
+//         .colorama()
+//         .diff(h.shape(200, 100))
+        
+        
+
+//     const [preset, setPreset] =useState(osc1)
+//     // const [slider1Value, setSlider1Value] =useState(1.00)
+//     // const [slider2Value, setSlider2Value] =useState(9)
+//     // const [slider3Value, setSlider3Value] =useState(0.01)
+//     // const [slider4Value, setSlider4Value] =useState(0.01)
    
    
-    return(
-        <div id='video-holder'>
-            {preset.rotate(rotate1)
-            .out()}
-            <button onClick={() => setRotate1(rotate1 + 0.05)}>Rotate</button>
-            <div id='buttons'>
-            <button onClick={() => {setPreset(osc1)}}>Osc 1</button>
-            <button onClick={() => {setPreset(osc2)}}>Osc2</button>
-            <button onClick={() => {setPreset(osc3)}}>Osc3</button>
-            <button onClick={() => {setPreset(osc4)}}>Osc4</button>
-        
-            </div>
+//     return(
+//         <div id='video-holder'>
+//             {preset
+//             .out()}
+//             <input type="range" min="2.00" max="100.00"  id="slider1" step='1.00'
+//             onChange={() => setSlider1Value(document.getElementById("slider1").value)}></input>
+//             {slider1Value}
+//             {/* <input type="range" min="1" max="90"  id="slider2" step='1'
+//             onInput={() => setSlider2Value(document.getElementById("slider2").value)}></input>
+//             {slider2Value}
+//             <input type="range" min="0.001" max="0.100"  id="slider3" step='0.001'
+//             onInput={() => setSlider3Value(document.getElementById("slider3").value)}></input>
+//             {slider3Value} */} 
+           
+            
+//             <div id='buttons'>
+//             <button onClick={() => {setPreset(osc1)}}>Osc 1</button>
+//             <button onClick={() => {setPreset(osc2)}}>Osc2</button>
+//             <button onClick={() => {setPreset(osc3)}}>Osc3</button>
+//             <button onClick={() => {setPreset(osc4)}}>Osc4</button>
+//             </div>
        
-        </div>
-    )
-}
-export default Hydraholder
+//         </div>
+//     )
+// }
+// export default Hydraholder
 
--- hydra-midi/src/Testvideo.js
+++ hydra-midi/src/Testvideo.js
@@ -1,24 +1,46 @@
-import { render } from "@testing-library/react";
-import React, { useEffect } from "react";
-import { useState } from 'react'
-import {useRef} from 'react'
+// import { render } from "@testing-library/react";
+// import React, { useEffect } from "react";
+// import { useState } from 'react'
+// import {useRef} from 'react'
 
-const Hydra = require('hydra-synth')  
-const h = new Hydra({ makeGlobal: true, detectAudio: false }).synth
+// const Hydra = require('hydra-synth')  
+// const h = new Hydra({ makeGlobal: true, numOutputs: 4, detectAudio: false }).synth
 
-function Testvideo() {
-    const [rotate1, setRotate1] =useState(1)
-    const osc1 = h.osc(10, 0.01, 1.4)
-    .rotate(rotate1)
-    .mult(h.osc(10, 0.1).modulate(h.osc(20).rotate(0, -0.01), 0.1))
-    .color(2.83, 0.91, 0.39)
+// function Testvideo() {
+//     const lfo = h.noise(4, 1.5, 10)
+//     const osc1 = h.noise(10, 0.01, 9.4)
+//     .modulate(lfo)
+//     .mult(h.osc((10, 0.1)).modulate(h.osc(20).rotate(0, -0.01), 0.1))
+//     .color(2.83, 0.91, 0.39)
+//     .scrollX(0.5, -0.1)
+//     .rotate(0.1, 0.1)
+//     .kaleid(4)
+//     .posterize()
+//     let osc2 = h.osc(80, 0).hue(1).colorama(2).contrast(2).kaleid(1).repeat(1).kaleid(200)
+//     .out(h.o1)
 
-    return(
-        <div>     
-        {osc1.out()}             
-        <button onClick={() => {setRotate1(rotate1 + 10)}}>button</button>
-        </div>
-    )
-}
+//     let osc3 = h.osc().modulate((h.o1)).diff((h.o1)).mult(h.shape(3, 5).repeat(2).rotate(-1,.2).kaleid(6)).colorama().diff(h.o1)
 
-export default Testvideo
+//     let osc4 = h.osc().color( ()=>cc[16], ()=>cc[17], ()=>cc[18] ).out())
+
+//     const mod2 = () => {
+//         osc3 = osc3.modulate(h.noise()).scrollX(0.5, 0.05).kaleid(10).rotate(.5, .4)
+//     }
+
+//     mod2()
+   
+    
+
+//     return(
+//         <div>          
+//         {/* <input type='range' min='-0.5' max='0.5' step='0.01' id='slideryo' onInput={() => setFreq(document.getElementById('slideryo').value)}></input>  */}
+//         {/* <input type='range' min='-10' max='10' step='1' id='slider2yo'onInput={() => setFreq2(document.getElementById('slider2yo').value)} ></input> 
+//         {osc2.out()}   */}
+//         {osc4.out()}
+
+//         {/* onInput={() => setFreq2(document.getElementById('slider2yo').value)}></input>  */}
+//         </div>
+//     )
+// }
+
+// export default Testvideo
